5ad60ce2afd41af95754f10ae6d0147ef510e9d5
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2025-11-02 23:24:20 +0800

    Change re-ranking mmodel

--- services/reranking.py
+++ services/reranking.py
@@ -1,12 +1,12 @@
-from cohere.client import Client
+from cohere.client_v2 import ClientV2
 from utils.logger import log
 from utils.utils  import get_envvar
 
-def connect_to_cohere_reranker() -> Client:
+def connect_to_cohere_reranker() -> ClientV2:
     """
     Connect to cohere client using API key.
     """
-    cohere_client = Client(api_key=get_envvar("COHERE_API_KEY"))
+    cohere_client = ClientV2(api_key=get_envvar("COHERE_API_KEY"))
     return cohere_client
 
 def rerank(query: str, documents: list, k: int = 10) -> list:
@@ -17,7 +17,7 @@ def rerank(query: str, documents: list, k: int = 10) -> list:
     log.info("INFO: Starting re-ranking process")
 
     rerank_response = cohere_client.rerank(
-        model="rerank-english-v3.0",  # Can try rerank-multilingual-v3.0
+        model="rerank-v3.5",  # Can try rerank-multilingual-v3.0
         query=query,
         documents=documents,
         top_n=min(k, len(documents)),
